391c9c4d0372cda3f7ecb3a7c4e11c68d1583e36
Ashraf Makki <makkiashraf08@gmail.com>
2021-12-16 17:04:46 -0500

    production code commit

--- selectionSort.java
+++ selectionSort.java
@@ -1,28 +1,27 @@
+
 public class selectionSort {
-
          /* Selection Sort function */
-
     public static void sort( int arr[] ){
 
         int N = arr.length;
 
         int i, j, temp;
-
-
         for (i = 0; i < N; i++)
-
         {
             for (j = i+1; j < N; j++)
 
             {
                 if (arr[i] > arr[j])
+
                 {
                 	  /* Swap arr[i] and arr[pos] */
                 	 temp = arr[i];
                 	 arr[i] = arr[j];
                 	 arr[j]= temp;
+
                 }
             }
-          }
-  }
+        }
+
+      }
 }
